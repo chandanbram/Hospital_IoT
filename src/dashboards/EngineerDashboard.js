d642746aa17f1c968115aee1ca7f4b5390ec0c4a
Chandan B Ram <106743828+chandanbram@users.noreply.github.com>
2025-07-30 13:27:58 +0530

    Update EngineerDashboard.js

--- src/dashboards/EngineerDashboard.js
+++ src/dashboards/EngineerDashboard.js
@@ -13,7 +13,7 @@ function EngineerDashboard() {
   const [performance, setPerformance] = useState("Accuracy: 94.3% | Loss: 0.12");
   const [trainingInProgress, setTrainingInProgress] = useState(false);
 
-  const AGGREGATOR_BASE_URL = 'https://decide-hit-graham-remaining.trycloudflare.com';
+  const AGGREGATOR_BASE_URL = 'https://apart-conjunction-volleyball-realize.trycloudflare.com';
 
   useEffect(() => {
     axios.get(`${AGGREGATOR_BASE_URL}/dp-status`)
